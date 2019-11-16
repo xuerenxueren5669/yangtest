bcb53a9412409a2c89c9fd18aaf63ad22fc52d0f
杨继伟 <yangjw@inspur.com>
2019-11-16 15:02:43 +0800

    加载引用新的包

--- src/app/animation/animationdemo-routing.module.ts
+++ src/app/animation/animationdemo-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {Animationcomponent} from './animationdemo/animationcomponent';
+import {TitleComponent} from 'yangjw-app';
 
 const routes: Routes = [
-  { path: '', component: Animationcomponent,data: {
+  { path: '',
+    component: Animationcomponent,
+    data: {
       breadcrumb: 'my-animation'
-    } },
+    },
+    children: [{
+      path: 'title',
+      component: TitleComponent
+    }]
+  }
+
 ];
 
 @NgModule({
--- src/app/animation/animationdemo.module.ts
+++ src/app/animation/animationdemo.module.ts
@@ -5,13 +5,14 @@ import { AnimationdemoRoutingModule } from './animationdemo-routing.module';
 import { Animationcomponent } from './animationdemo/animationcomponent';
 import {TestmModule} from 'yangjw-app';
 import {CartoonModule} from 'yangcartoon-app/cartoon.module';
-
+import {CoreModule} from 'yangjw-app';
 @NgModule({
   imports: [
     CommonModule,
     AnimationdemoRoutingModule,
     TestmModule,
-    CartoonModule
+    CartoonModule,
+    CoreModule.forRoot({"userName":"12345"})
   ],
   declarations: [Animationcomponent]
 })
